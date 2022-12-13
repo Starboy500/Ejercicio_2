9993edf3ca3fd857d57c56712bce54c1d3532adb
Starboy500 <120419943+Starboy500@users.noreply.github.com>
2022-12-12 21:53:56 -0600

    Add files via upload

---
+++ Ejercicio_2/index.js
@@ -0,0 +1,16 @@
+
+
+const fechaNacimiento = new Date(1997,9,2);
+const fechaL = new Date(1841,3);
+const librofavorito = {
+    titulo:"Los crimenes de la calle morgue",
+    autor:"Edgar Allan Poe",
+    fecha: fechaL,
+    enlace: "http//bibliotecadigital.ilce.edu.mx/Colecciones/ObrasClasicas/_docs/CrimenesCalleMorgue.pdf"
+};
+
+const ar = ["israel",25,false,fechaNacimiento,librofavorito];
+
+console.log(ar);
+
+
